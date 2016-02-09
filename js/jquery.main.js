c834c7a9250fa40b8f6b8a575417ab593f174a39
Sergey <kuzmenkos1986@gmail.com>
2016-02-09 16:53:58 +0200

    last

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,8 +1,49 @@
 $(function(){
-
+    if (window.devicePixelRatio > 1) {
+        $('img').each(function () {
+            this.src = this.getAttribute('data-full');
+        });
+    }
     $('.click-title').click(function () {
         $('.click-title').removeClass('click-title_1');
         $(this).addClass('click-title_1')
+    });
+
+    $('.panel').click(function () {
+        $('.panel').children("img").css("display" , "none");
+        $(this).children("img").css("display" , "block");
+    });
+
+    $(".nav a").click(function (){
+
+        var curElem = $(this),
+            curClass = curElem.attr('data-class');
+
+        $('html, body').animate({
+            scrollTop: $('.' + curClass).offset().top - 125
+        }, 1000);
+        return false;
+    });
+
+    $('.btn_up').on({
+        'click':function(){
+            $('html, body').animate({scrollTop: 0}, 600);
+        }
+    });
+
+    $(window).on({
+        'scroll':function(){
+            if($(window).scrollTop() > $(window).height()){
+                $('.btn_up').fadeIn();
+            } else {
+                $('.btn_up').fadeOut();
+            }
+            if($(window).scrollTop() + $(window).height() > $('.site__footer').offset().top + 16) {
+                $('.btn_up').css({ bottom: $(window).scrollTop() + $(window).height() - $('footer').offset().top + 16 });
+            } else {
+                $('.btn_up').css({ bottom: '16px' });
+            }
+        }
     });
 
 } );
@@ -33,14 +74,6 @@ Shablon.prototype = {
         };
     }
 };
-
-$('#slide_up').on({
-    'click':function(){
-        $('html, body').animate({scrollTop: 0}, 600);
-    }
-});
-
-
 
 $(window).on({
     load: function () {
